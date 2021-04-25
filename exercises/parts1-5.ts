cbdd5fc683aeca1d263a7cd6230e53a519137adf
Lorena Sarasua <lorena.sarasua@gmail.com>
2021-04-24 21:38:13 -0400

    define a function that calculates the days it would take to travel to a location.

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -16,15 +16,17 @@ let milesToMars: number =  kilometersToMars * milesPerKilometer;
 let hoursToMars: number = milesToMars / speedMph;
 let daysToMars: number = hoursToMars / 24;
 
-console.log(`${spacecraftName} would take ${daysToMars} days to get to Mars.`);
-
 // Code an output statement here (use a template literal):
 
-
+console.log(`${spacecraftName} would take ${daysToMars} days to get to Mars.`);
 
 // Part 3: Create a Function ("getDaysToLocation")
 
-
+function getDaysToLocation(kilometersAway: number): number {
+  let milesAway: number = kilometersAway * milesPerKilometer;
+  let hoursToLocation: number = milesAway / speedMph;
+  return hoursToLocation / 24;
+}
 
 // Move your output statement from part 2 here. Update the template literal to call
 // the function and print the outputs for a Mars trip and a moon trip.
